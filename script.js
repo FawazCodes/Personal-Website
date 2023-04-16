5fc8edd0b164cc5229541b4c431cad029e507884
FawazCodes <108174119+FawazCodes@users.noreply.github.com>
2023-04-16 02:28:22 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+
+btn1.addEventListener('click', () => {
+  window.location.href = 'https://fawazcodes.github.io/number-guessing-game/';
+});
+
+btn2.addEventListener('click', () => {
+  window.location.href = 'https://curiouscat.live/Idkwallah';
+});
