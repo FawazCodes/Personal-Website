c00661ab44e2d441f6e00ef7892aa9d43e205fb3
FawazCodes <108174119+FawazCodes@users.noreply.github.com>
2023-04-16 03:53:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const redirectBtn = document.getElementById('redirectBtn');
-    const gameBtns = document.querySelectorAll('.game-btn');
-    const result = document.getElementById('result');
+const result = document.getElementById("result");
+const gameButtons = document.querySelectorAll(".game-btn");
+const redirectBtn = document.getElementById("redirectBtn");
+const winAudio = document.getElementById("winAudio");
 
-    redirectBtn.addEventListener('click', () => {
-        window.location.href = 'https://curiouscat.live/Idkwallah';
-    });
-
-    gameBtns.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-            const playerChoice = e.target.getAttribute('data-choice');
-            const computerChoice = getComputerChoice();
-            const gameResult = playGame(playerChoice, computerChoice);
-            result.textContent = gameResult;
-        });
-    });
-
-    function getComputerChoice() {
-        const choices = ['rock', 'paper', 'scissors'];
-        const randomIndex = Math.floor(Math.random() * choices.length);
-        return choices[randomIndex];
-    }
-
-    function playGame(playerChoice, computerChoice) {
-        if (playerChoice === computerChoice) {
-            return `It's a draw! You both chose ${playerChoice}.`;
-        }
-
-        if (
-            (playerChoice === 'rock' && computerChoice === 'scissors') ||
-            (playerChoice === 'paper' && computerChoice === 'rock') ||
-            (playerChoice === 'scissors' && computerChoice === 'paper')
-        ) {
-            return `You win! ${playerChoice} beats ${computerChoice}.`;
-        } else {
-            return `You lose! ${computerChoice} beats ${playerChoice}.`;
-        }
-    }
+redirectBtn.addEventListener("click", () => {
+    window.location.href = "https://curiouscat.live/Idkwallah";
 });
+
+gameButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const userChoice = button.dataset.choice;
+        const computerChoice = getRandomChoice();
+        const outcome = determineWinner(userChoice, computerChoice);
+        result.textContent = `You chose ${userChoice}, computer chose ${computerChoice}. ${outcome}`;
+    });
+});
+
+function getRandomChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+function playWinAudio() {
+    winAudio.currentTime = 0;
+    winAudio.play();
+}
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return "It's a tie!";
+    }
+
+    if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "scissors" && computerChoice === "paper") ||
+        (userChoice === "paper" && computerChoice === "rock")
+    ) {
+        playWinAudio();
+        return "You win!";
+    }
+
+    return "You lose!";
+}
