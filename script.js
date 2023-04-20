b5ae575d46a194ed72f4c4187f168e672f2ab2f9
FawazCodes <108174119+FawazCodes@users.noreply.github.com>
2023-04-19 21:33:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const result = document.getElementById("result");
 const gameButtons = document.querySelectorAll(".game-btn");
 const redirectBtn = document.getElementById("redirectBtn");
-const winAudio = new Audio("win-sound.mp3");
 
 // Add event listeners
 redirectBtn.addEventListener("click", () => {
@@ -28,7 +27,21 @@ function getRandomChoice() {
 }
 
 function playWinAudio() {
-    winAudio.currentTime = 0;
+    const winSounds = [
+        "win-sound.mp3",
+        "win-sound2.mp3",
+        "win-sound3.mp3",
+        "win-sound4.mp3",
+        "win-sound5.mp3",
+        "win-sound6.mp3",
+        "win-sound7.mp3",
+        "win-sound8.mp3",
+        "win-sound9.mp3",
+        "win-sound10.mp3",
+    ];
+    const randomIndex = Math.floor(Math.random() * winSounds.length);
+    const winAudio = new Audio(winSounds[randomIndex]);
+    winAudio.loop = true;
     winAudio.play();
 }
 
@@ -36,6 +49,7 @@ function determineWinner(userChoice, computerChoice) {
     console.log(`User choice: ${userChoice}, Computer choice: ${computerChoice}`);
     if (userChoice === computerChoice) {
         result.style.color = ""; // Reset the text color to the default
+        document.body.style.background = "black"; // Set the background to black
         return "It's a tie!";
     }
 
@@ -47,10 +61,13 @@ function determineWinner(userChoice, computerChoice) {
         playWinAudio();
         console.log("You win!");
         result.style.color = "limegreen"; // Change the text color to green
+        document.body.style.background = "url('background.gif') no-repeat center center fixed"; // Set the background to the GIF
+        document.body.style.backgroundSize = "cover";
         return "You win!";
     }
 
     result.style.color = ""; // Reset the text color to the default
+    document.body.style.background = "black"; // Set the background to black
     console.log("You lose!");
     return "You lose!";
 }
