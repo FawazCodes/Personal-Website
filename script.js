c38442f91c6b426d6a9c4ce1bf1439c8b393fb7c
FawazCodes <108174119+FawazCodes@users.noreply.github.com>
2023-04-27 08:08:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,89 +23,102 @@ document.addEventListener("DOMContentLoaded", function () {
   let consecutiveTieOrLossCount = 0;
 
   function playRound(playerChoice) {
-    const computerChoice = (function () {
-      const choices = ["rock", "paper", "scissors"];
-      return choices[Math.floor(Math.random() * choices.length)];
-    })();
+  const computerChoice = (function () {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+  })();
 
-    const result = (function () {
-      if (playerChoice === computerChoice) {
-        return "tie";
-      } else if (
-        (playerChoice === "rock" && computerChoice === "scissors") ||
-        (playerChoice === "paper" && computerChoice === "rock") ||
-        (playerChoice === "scissors" && computerChoice === "paper")
-      ) {
-        return "win";
-      } else {
-        return "lose";
-      }
-    })();
-    
-    if (result === "win") {
-      let i;
-      do {
-        i = Math.floor(Math.random() * images.length);
-      } while (!images[i].loaded);
-
-      const image = images[i];
-      const audio = new Audio();
-      audio.preload = "auto";
-      audio.src = `win-sound${Math.floor(Math.random() * 4) + 1}.mp3`;
-      audio.play();
-
-      document.body.style.backgroundImage = `url('${image.src}')`;
-    }
-
-    let resultTextContent, resultEmoji, resultColor;
-    if (result === "tie") {
-      resultTextContent = "It's a Tie";
-      resultEmoji = "😐";
-      resultColor = "white";
-    } else if (result === "win") {
-      resultTextContent = "You Won!!!";
-      resultEmoji = "🥳";
-      resultColor = "green";
-      resultText.style.animation = "win 0.5s ease-out";
+  const result = (function () {
+    if (playerChoice === computerChoice) {
+      return "tie";
+    } else if (
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "paper" && computerChoice === "rock") ||
+      (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+      return "win";
     } else {
-      resultTextContent = "You Lost...";
-      resultEmoji = "😢";
-      resultColor = "red";
-      resultText.style.animation = "lose 0.5s ease-out";
+      return "lose";
     }
+  })();
 
-    resultText.innerHTML = `${resultTextContent} ${resultEmoji}<br>Computer chose ${computerChoice} ${
-      computerChoice === "rock" ? "🪨" : computerChoice === "paper" ? "📄" : "✂️"
-    }`;
-    resultText.style.color = resultColor;
+  if (result === "win") {
+    let i;
+    do {
+      i = Math.floor(Math.random() * images.length);
+    } while (!images[i].loaded);
 
-    if (result === "tie" || result === "lose") {
-      if ((previousResult === "tie" || previousResult === "lose") && result !== "win") {
-        consecutiveTieOrLossCount++;
-      } else {
-        consecutiveTieOrLossCount = 1;
-      }
-    } else {
-      consecutiveTieOrLossCount = 0;
-    }
+    const image = images[i];
+    const audio = new Audio();
+    audio.preload = "auto";
+    audio.src = `win-sound${Math.floor(Math.random() * 4) + 1}.mp3`;
+    audio.play();
 
-    previousResult = result;
+    document.body.style.backgroundImage = `url('${image.src}')`;
 
-      if (consecutiveTieOrLossCount >= 2) {
-      resultText.style.animation = "shake 0.82s cubic-bezier(.36,.07,.19,.97)";
-    } else {
-      resultText.style.animation = "";
-    }
-
-    if (result === "win") {
-      resultText.style.animation = "pop 1s ease-in-out forwards";
-      setTimeout(function () {
-        resultText.classList.remove("pop");
-      }, 1000);
-    } else {
-      resultText.classList.remove("pop");
-    }
+    // add hilarious effect on win
+    const winEffects = [
+      "stretch",
+      "dance",
+      "sling",
+      "rotate"
+    ];
+    const randomEffect = winEffects[Math.floor(Math.random() * winEffects.length)];
+    resultText.classList.add(randomEffect);
+    setTimeout(function () {
+      resultText.classList.remove(randomEffect);
+    }, 1000);
   }
+
+  let resultTextContent, resultEmoji, resultColor;
+  if (result === "tie") {
+    resultTextContent = "It's a Tie";
+    resultEmoji = "😐";
+    resultColor = "white";
+  } else if (result === "win") {
+    resultTextContent = "You Won!!!";
+    resultEmoji = "🥳";
+    resultColor = "green";
+    resultText.style.animation = "win 0.5s ease-out";
+  } else {
+    resultTextContent = "You Lost...";
+    resultEmoji = "😢";
+    resultColor = "red";
+    resultText.style.animation = "lose 0.5s ease-out";
+  }
+
+  resultText.innerHTML = `${resultTextContent} ${resultEmoji}<br>Computer chose ${computerChoice} ${
+    computerChoice === "rock" ? "🪨" : computerChoice === "paper" ? "📄" : "✂️"
+  }`;
+  resultText.style.color = resultColor;
+
+  if (result === "tie" || result === "lose") {
+    if ((previousResult === "tie" || previousResult === "lose") && result !== "win") {
+      consecutiveTieOrLossCount++;
+    } else {
+      consecutiveTieOrLossCount = 1;
+    }
+  } else {
+    consecutiveTieOrLossCount = 0;
+  }
+
+  previousResult = result;
+
+  if (consecutiveTieOrLossCount >= 2) {
+    resultText.style.animation = "shake 0.82s cubic-bezier(.36,.07,.19,.97)";
+  } else {
+    resultText.style.animation = "";
+  }
+
+  if (result === "win") {
+    resultText.style.animation = "pop 1s ease-in-out forwards";
+    setTimeout(function () {
+      resultText.classList.remove("pop");
+    }, 1000);
+  } else {
+    resultText.classList.remove("pop");
+  }
+}
 
   images.forEach(function (image) {
     const img = new Image();
