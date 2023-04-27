cf0d816848eb6a2e7ab7763009f6cacdaf1c39b2
FawazCodes <108174119+FawazCodes@users.noreply.github.com>
2023-04-27 13:30:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,95 @@
 document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("curiousCat").onclick = function () {
+    window.location.href = "https://curiouscat.live/Idkwallah";
+  };
+
   const rockButton = document.getElementById("rock");
   const paperButton = document.getElementById("paper");
   const scissorsButton = document.getElementById("scissors");
   const resultText = document.getElementById("result");
-  const curiousCatButton = document.getElementById("curiousCat");
-  const winEffects = ["stretch", "dance", "sling", "rotate", "bounce", "flip"];
-
-  const images = [
-    { src: "background.gif", loaded: false },
-    { src: "background2.gif", loaded: false },
-  ];
-
-  images.forEach((image) => {
-    const img = new Image();
-    img.onload = () => (image.loaded = true);
-    img.onerror = () => console.error(`Error loading ${img.src}`);
-    img.src = image.src;
-    image.img = img;
-  });
-
-  curiousCatButton.onclick = () => {
-    window.location.href = "https://curiouscat.live/Idkwallah";
-  };
-
-  rockButton.addEventListener("click", () => playRound("rock"));
-  paperButton.addEventListener("click", () => playRound("paper"));
-  scissorsButton.addEventListener("click", () => playRound("scissors"));
 
   let consecutiveTieOrLossCount = 0;
 
-  function playRound(playerChoice) {
-    const choices = ["rock", "paper", "scissors"];
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    const result = getResult(playerChoice, computerChoice);
+  const winEffects = [
+    "stretch",
+    "dance",
+    "sling",
+    "rotate",
+    "bounce",
+    "flip"
+  ];
 
-    if (result === "win") {
-      handleWin();
-    } else {
-      handleShake();
-    }
-
-    updateResultText(result, playerChoice, computerChoice);
-    consecutiveTieOrLossCount = updateTieOrLossCount(result);
+  function getRandomEffect() {
+    return winEffects[Math.floor(Math.random() * winEffects.length)];
   }
 
-  function getResult(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) return "tie";
-    if (
-      (playerChoice === "rock" && computerChoice === "scissors") ||
-      (playerChoice === "paper" && computerChoice === "rock") ||
-      (playerChoice === "scissors" && computerChoice === "paper")
-    ) return "win";
-    return "lose";
-  }
-
-  function handleWin() {
-    const randomImageIndex = Math.floor(Math.random() * images.length);
-    const randomEffectIndex = Math.floor(Math.random() * winEffects.length);
-    const randomAudioIndex = Math.floor(Math.random() * 4) + 1;
-
-    document.body.style.backgroundImage = `url('${images[randomImageIndex].src}')`;
-    resultText.classList.add(winEffects[randomEffectIndex]);
-    new Audio(`win-sound${randomAudioIndex}.mp3`).play();
+  function applyRandomEffect() {
+    const randomEffect = getRandomEffect();
+    resultText.classList.add(randomEffect);
 
     setTimeout(() => {
-      resultText.classList.remove(winEffects[randomEffectIndex]);
+      resultText.classList.remove(randomEffect);
+      applyRandomEffect();
     }, 3000);
   }
 
-  function handleShake() {
-    if (consecutiveTieOrLossCount >= 1) {
+  function playRound(playerChoice) {
+    const computerChoice = generateComputerChoice();
+    const result = determineResult(playerChoice, computerChoice);
+
+    if (result === "win") {
+      applyRandomEffect();
+    } else {
       resultText.classList.add("shake");
 
       setTimeout(() => {
         resultText.classList.remove("shake");
-      }, 820);
+      }, 3000);
+    }
+
+    updateResultText(result, computerChoice);
+    consecutiveTieOrLossCount = updateTieOrLossCount(result);
+  }
+
+  function generateComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+  }
+
+  function determineResult(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+      return "tie";
+    } else if (
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "paper" && computerChoice === "rock") ||
+      (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+      return "win";
+    } else {
+      return "lose";
     }
   }
 
-  function updateResultText(result, playerChoice, computerChoice) {
-    const emojiMap = {
-      tie: "😐",
-      win: "🥳",
-      lose: "😢",
+  function updateResultText(result, computerChoice) {
+    const resultTextMap = {
+      tie: "It's a Tie 😐",
+      win: "You Won!!! 🥳",
+      lose: "You Lost... 😢",
     };
+
     const choiceEmojiMap = {
       rock: "🪨",
       paper: "📄",
       scissors: "✂️",
     };
 
-    const resultTextMap = {
-      tie: "It's a Tie",
-      win: "You Won!!!",
-      lose: "You Lost...",
-    };
-
-       const resultTextColorMap = {
+    const resultTextColorMap = {
       tie: "white",
       win: "green",
       lose: "red",
     };
 
-    resultText.innerHTML = `${resultTextMap[result]} ${emojiMap[result]}<br>Computer chose ${computerChoice} ${choiceEmojiMap[computerChoice]}`;
+    resultText.innerHTML = `${resultTextMap[result]}<br>Computer chose ${computerChoice} ${choiceEmojiMap[computerChoice]}`;
     resultText.style.color = resultTextColorMap[result];
   }
 
@@ -112,4 +99,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     return 0;
   }
+
+  rockButton.addEventListener("click", function () {
+    playRound("rock");
+  });
+
+  paperButton.addEventListener("click", function () {
+    playRound("paper");
+  });
+
+  scissorsButton.addEventListener("click", function () {
+    playRound("scissors");
+  });
 });
